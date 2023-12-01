2f50d6beba4af8d733b8ffefd1ddfb07cc5224d8
wxy <102939294+noBaldAaa@users.noreply.github.com>
2023-12-01 11:38:18 +0800

    在 dist 目录下手动生成一个 html 模版文件，并看 react 有没有成功生效

--- README.md
+++ README.md
@@ -9,3 +9,4 @@
 - npx tsx --init
 - 修改部分配置 yarn add @types/react @types/react-dom
 - 配置基础的 esbuild 配置文件，并尝试打包
+- 在 dist 目录下手动生成一个 html 模版文件，并看 react 有没有成功生效
--- main.tsx
+++ main.tsx
@@ -2,11 +2,7 @@ import { createRoot } from "react-dom/client";
 import React from "react";
 
 const App = () => {
-  return (
-    <div>
-      <span className="iconfont icon-delet"></span>
-    </div>
-  );
+  return <div>hello esbuild</div>;
 };
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
